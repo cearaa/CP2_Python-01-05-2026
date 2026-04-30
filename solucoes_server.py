f9ae7814b6f68386ecf399f4e76802ddd521ef02
Rikk <tarikmoussa90@gmail.com>
2026-04-30 12:41:28 -0300

    feat: defs p/menu

--- solucoes_server.py
+++ solucoes_server.py
@@ -7,12 +7,22 @@
 #Carlos Eduardo - RM: 569574
 #Ítalo Neto - RM: 572912
 
+sistema_escolhido = "Kali Linux"
+
 #Função para exibir opções
 def mostrar_menu():
     print('1. Windows Server 2022 ')
     print('2. Ubuntu Server ')
-    print('3. Red Hat Enterprise Linux (RHEL)')
-    print('4. Kali Linux\n')
-    print('0. Encerrar votação ')
+    print('3. Red Hat Enterprise Linux (RHEL) ')
+    print(f'4. {sistema_escolhido} ')
+    print('0. Encerrar votação\n ')
+
+#validas os vots
+def validar_voto(numVoto):
+    return 0 <= numVoto <= 4
+
+#contados do voto pela regra do return numVoto
+votos1 = votos2 = votos3 = votos4 = 0
+
 
 
