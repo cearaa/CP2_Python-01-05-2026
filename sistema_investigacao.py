c444c58192689977b0cc906d20845cb54d6e5079
Rikk <tarikmoussa90@gmail.com>
2026-04-30 11:52:56 -0300

    feat: estrutura do while e def perg

--- sistema_investigacao.py
+++ sistema_investigacao.py
@@ -21,7 +21,7 @@ def fazer_pergunta(numero):
         elif numero == 4:
             pergunta = input("Você tinha dívidas, emprestou ou circulava dinheiro com o Art? (s/n): ")
         elif numero == 5:
-            pergunta = input("Art era um bom comerciante. Você já trabalhou com ou usurfruia dos produtos dele? (s/n): ")
+            pergunta = input("Art era um bom comerciante. Você já trabalhou com ou usufruía dos produtos dele? (s/n): ")
         else:
             return 0
 
@@ -37,9 +37,9 @@ def fazer_pergunta(numero):
 #Função pedida no ex
 def classificar_investigado(total_sim):
     if total_sim == 5:
-        return "As evideências são claras... Você é o Assassino!!!"
+        return "As evidências são claras... Você é o Assassino!!!"
     elif total_sim >= 3:
-        return "Após a análise de recursos sêmióticos, você foi entitulado como Cumplíce."
+        return "Após a análise de recursos sêmióticos, você foi entitulado como Cúmplice."
     elif total_sim == 2:
         return "Por pouco... Você é Suspeito, mas se prepare para mais perguntas.."
     else:
@@ -56,3 +56,10 @@ while contador <= 5:
 
 print("\nTotal de respostas sim para mérito de classificação: ", total_positivo)
 print("Classificação: ", classificar_investigado(total_positivo))
+print("Após bruta analíse das suas respostas. Pela quantidade, ausência ou presença de sim, chegamos nessa conclusão.")
+
+
+#README, tópico de entrega do exercício do CP. Explicação de onde foram usadas repetição, condicionais e função.
+#As funções aparecem nas linhas 13 e 38. o Objetivo de cada uma atende ao seu nome; a da linha 13 mostra o menu do sistema e imprimi as perguntas, usando uma estrutura de repetição while True, para que a resposta siga e absorva o que ela pede, no caso " s/n ", caso contrário, faz um retorno e imprimi a mensagem de erro, com formato incorreto de mensagem.
+#A função da linha 38 serve para classificar o usuario de acorco com quantos sim foram respondidos na def 13, e essas impressões são armazenaas e reultilizadas para saber se realmente atendem àquela circunstância, pelo return.
+#As condicioonais if e else nas duas def, são usadas junto de uma váriavel que a gente preescreveu e,
