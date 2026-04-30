591a055d146225636505669afc3882fda088f2a3
Rikk <tarikmoussa90@gmail.com>
2026-04-30 10:52:07 -0300

    feat estrutura do while e def perg

---
+++ sistema_investigacao.py
@@ -0,0 +1,11 @@
+#Integrantes da Challenge
+#Tárik Moussa Alma - RM: 571411
+#Giovanni Azevedo - RM: 569750
+#Fabricio Aquiles Sales da Silva - RM: 570985
+#Carlos Eduardo - RM: 569574
+#Ítalo Neto - RM: 572912
+
+def fazer_pergunta(numero):
+    while True:
+        if numero == 1:
+            resposta = input("Você telefonou para a vítima entre o tempo do assassinato? ")
