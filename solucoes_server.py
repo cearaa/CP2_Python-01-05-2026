28f48cefe0fd8e1984868fc4b862e6e280e6d1b7
Rikk <tarikmoussa90@gmail.com>
2026-04-30 12:31:25 -0300

    fix: inicialização da main p2

---
+++ solucoes_server.py
@@ -0,0 +1,9 @@
+#Parte 2 do CP2.
+
+#Integrantes da Challenge/CP
+#Tárik Moussa Alma - RM: 571411
+#Giovanni Azevedo - RM: 569750
+#Fabricio Aquiles Sales da Silva - RM: 570985
+#Carlos Eduardo - RM: 569574
+#Ítalo Neto - RM: 572912
+
