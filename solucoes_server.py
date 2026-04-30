9deaf6918ed658e28a6f377aebb793741eef1d2b
Rikk <tarikmoussa90@gmail.com>
2026-04-30 12:48:32 -0300

    feat: finalização exercício 1 parte II

--- solucoes_server.py
+++ solucoes_server.py
@@ -24,6 +24,44 @@ def validar_voto(numVoto):
 #contados do voto pela regra do return numVoto
 votos1 = votos2 = votos3 = votos4 = 0
 
+#loop de votacao
+while True:
+    mostrar_menu()
+    try:
+        voto = int(input("Digite seu voto: "))
+    except:
+        print("Entrada inválida!! Digite um número. ;)")
+        continue
+    # Validação
+    if not validar_voto(voto):
+        print("Voto inválido!! Tente novamente. ;)")
+        continue
+    #encerrar votacao
+    if voto == 0:
+        break
+    #contabilizar votos
+    if voto == 1:
+        votos1 += 1
+    elif voto == 2:
+        votos2 += 1
+    elif voto == 3:
+        votos3 += 1
+    elif voto == 4:
+        votos4 += 1
+
+#res
+total = votos1 + votos2 + votos3 + votos4
+
+print("\n============================= RESULTADO FINAL ==========================")
+print(f"Windows Server 2022: {votos1} votos")
+print(f"Ubuntu Server: {votos2} votos")
+print(f"RHEL: {votos3} votos")
+print(f"{sistema_escolhido}: {votos4} votos")
+print(f"Total de votos: {total}")
+
+
+#O while mantém o programa em execução, repetindo a coleta de votos até que digitem 0,/
+#que é a condição usada para encerrar o loop com o break.
 
 
 
